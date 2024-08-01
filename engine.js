83252342375426eab96bcfff7e6c2de8b829a356
odnols <gusbra21@outlook.com>
2024-08-01 20:14:38 -0300

    Polidas e ajustes

--- engine.js
+++ engine.js
@@ -8,6 +8,8 @@ canvas.height = 110
 const image = new Image(85, 85)
 image.src = "./brasao.png"
 
+const logo = new Image(90, 90)
+
 const siglas = {
     "de": [18, 75, "65px"],
     "dc": [15, 75, "65px"],
@@ -33,9 +35,9 @@ function atualiza_canvas() {
     ctx.clearRect(0, 0, 910, 110)
     ctx.restore()
 
-    const cor_links = document.getElementById("check_cor_links").value
-    const cor_linha = document.getElementById("check_cor_linha").value
-    const cor_destaque = document.getElementById("check_cor_destaque").value
+    const cor_links = get("check_cor_links").value
+    const cor_linha = get("check_cor_linha").value
+    const cor_destaque = get("check_cor_destaque").value
 
     let cor_titulo = hexToRgb(cor_destaque)
     cor_titulo = `rgb(${cor_titulo.r - 21}, ${cor_titulo.g - 20}, ${cor_titulo.b - 20})`
@@ -44,7 +46,14 @@ function atualiza_canvas() {
     cor_destaque_2 = `rgb(${cor_destaque_2.r - 31}, ${cor_destaque_2.g - 22}, ${cor_destaque_2.b - 22})`
 
     if (cor_destaque !== "#dd3333" || cor_linha !== "#616161" || cor_links !== "#2b63d4")
-        document.getElementById("cor_padrao").style.display = "block"
+        get("cor_padrao").style.display = "block"
+
+    if ((get("cargo").value).toLowerCase().includes("chefe"))
+        get("select_faixa_superior").style.display = "block"
+    else {
+        get("faixa_superior").checked = false
+        get("select_faixa_superior").style.display = "none"
+    }
 
     ctx.shadowOffsetX = 3
     ctx.shadowOffsetY = 3
@@ -65,34 +74,35 @@ function atualiza_canvas() {
     ctx.shadowColor = "transparent"
     ctx.shadowBlur = 2
 
-    if (document.getElementById("sub_sombra").checked) ctx.shadowColor = "rgba(0, 0, 0, .3)"
+    if (get("sub_sombra").checked) ctx.shadowColor = "rgba(0, 0, 0, .3)"
 
     ctx.fillStyle = cor_destaque
     ctx.fillRect(5, 5, 90, 90)
 
     ctx.fillStyle = "white"
-    ctx.font = `${siglas[document.getElementById("sigla").value][2]} Impact`
+    ctx.font = `${siglas[get("sigla").value][2]} Impact`
 
-    ctx.fillText((document.getElementById("sigla").value).toUpperCase(), siglas[document.getElementById("sigla").value][0], siglas[document.getElementById("sigla").value][1])
+    ctx.fillText((get("sigla").value).toUpperCase(), siglas[get("sigla").value][0], siglas[get("sigla").value][1])
     ctx.shadowColor = "transparent"
 
     ctx.font = "bold 16px Verdana"
     ctx.fillStyle = cor_titulo
-    ctx.fillText(document.getElementById("nome").value, 110, 25)
+    ctx.fillText(get("nome").value, 110, 25)
 
     ctx.drawImage(image, 460, 15, image.width, image.height)
+    ctx.drawImage(logo, 5, 5, logo.width, logo.height)
 
-    const emojis = document.getElementById("emojis").checked ? true : false
+    const emojis = get("emojis").checked ? true : false
 
-    if ((document.getElementById("cargo").value).length > 0) {
+    if ((get("cargo").value).length > 0) {
         ctx.font = "13px Verdana"
         ctx.fillStyle = cor_linha
-        ctx.fillText(document.getElementById("cargo").value, 110, 45)
+        ctx.fillText(get("cargo").value, 110, 45)
     }
 
-    if ((document.getElementById("sigla").value).length > 0) {
+    if ((get("sigla").value).length > 0) {
 
-        const e = document.getElementById("sigla")
+        const e = get("sigla")
 
         ctx.font = "13px Verdana"
         ctx.fillStyle = cor_titulo
@@ -101,28 +111,28 @@ function atualiza_canvas() {
         ctx.shadowColor = "transparent"
     }
 
-    if ((document.getElementById("subdivisao").value).length > 0 && document.getElementById("sub_div_check").checked) {
+    if ((get("subdivisao").value).length > 0 && get("sub_div_check").checked) {
         ctx.font = "13px Verdana"
         ctx.fillStyle = cor_titulo
-        ctx.fillText(document.getElementById("subdivisao").value, 110, 85)
+        ctx.fillText(get("subdivisao").value, 110, 85)
     }
 
-    if ((document.getElementById("email").value).length > 0) {
+    if ((get("email").value).length > 0) {
         ctx.font = "13px Verdana"
         ctx.fillStyle = cor_links
-        ctx.fillText(`${emojis ? "📧 " : ""}${document.getElementById("email").value}`, 550, 25)
+        ctx.fillText(`${emojis ? "📧 " : ""}${get("email").value}`, 550, 25)
     }
 
-    if ((document.getElementById("telefone").value).length > 0) {
+    if ((get("telefone").value).length > 0) {
         ctx.font = "11px Verdana"
         ctx.fillStyle = cor_linha
-        ctx.fillText(`${emojis ? " 📞 " : ""}${document.getElementById("telefone").value}`, 550, 45)
+        ctx.fillText(`${emojis ? " 📞 " : ""}${get("telefone").value}`, 550, 45)
     }
 
-    if ((document.getElementById("endereco").value).length > 0) {
+    if ((get("endereco").value).length > 0) {
         ctx.font = "11px Verdana"
         ctx.fillStyle = "black"
-        ctx.fillText(`${emojis ? "  📍  " : ""}${document.getElementById("endereco").value}`, 550, 65)
+        ctx.fillText(`${emojis ? "  📍  " : ""}${get("endereco").value}`, 550, 65)
     }
 
     ctx.font = "12px Verdana"
@@ -134,7 +144,7 @@ function atualiza_canvas() {
     ctx.strokeStyle = cor_destaque
     ctx.lineWidth = 2
 
-    if (document.getElementById("sub_sombra").checked) ctx.shadowColor = "rgba(0, 0, 0, .2)"
+    if (get("sub_sombra").checked) ctx.shadowColor = "rgba(0, 0, 0, .2)"
 
     ctx.shadowOffsetX = 2
     ctx.shadowOffsetY = 2
@@ -160,7 +170,7 @@ function atualiza_canvas() {
 
     ctx.stroke()
 
-    if (document.getElementById("borda_triangular").checked) {
+    if (get("borda_triangular").checked) {
 
         ctx.shadowOffsetX = -5
         ctx.shadowOffsetY = 0
@@ -172,15 +182,22 @@ function atualiza_canvas() {
         ctx.lineTo(901, 0)
         ctx.fill()
 
-        if ((document.getElementById("cargo").value).includes("Chefe")) {
+        if (get("faixa_superior").checked) {
 
             ctx.strokeStyle = cor_destaque
             ctx.lineWidth = 10
+
+            ctx.shadowOffsetX = -10
+            ctx.shadowOffsetY = -10
 
             ctx.beginPath()
             ctx.moveTo(0, 0)
             ctx.lineTo(901, 0)
             ctx.stroke()
+
+            ctx.lineWidth = 2
+            ctx.shadowOffsetX = -5
+            ctx.shadowOffsetY = 0
         }
 
         ctx.fillStyle = cor_destaque
@@ -207,11 +224,63 @@ function hexToRgb(hex) {
 
 function redefine_cor() {
 
-    document.getElementById("check_cor_destaque").value = "#dd3333"
-    document.getElementById("check_cor_links").value = "#2b63d4"
-    document.getElementById("check_cor_linha").value = "#909090"
+    get("check_cor_destaque").value = "#dd3333"
+    get("check_cor_links").value = "#2b63d4"
+    get("check_cor_linha").value = "#616161"
     atualiza_canvas()
 
-    document.getElementById("sub_cor_padrao").checked = false
-    document.getElementById("cor_padrao").style.display = "none"
+    get("sub_cor_padrao").checked = false
+    get("cor_padrao").style.display = "none"
 }
+
+function previewImage() {
+
+    let file = get("logo").files
+
+    if (file.length > 0) {
+        let fileReader = new FileReader()
+
+        fileReader.onload = function (event) {
+            logo.src = event.target.result
+            atualiza_canvas()
+        }
+
+        fileReader.readAsDataURL(file[0])
+    }
+}
+
+function get(alvo) {
+
+    let alvos = document.getElementsByClassName(alvo)
+
+    if (alvos.length < 1)
+        alvos = document.getElementById(alvo)
+
+    return alvos
+}
+
+function mudar_tema(auto) {
+
+    let tema_atual = localStorage.getItem("tema_rodape")
+
+    if (parseInt(tema_atual)) {
+        get("entradas").style.backgroundColor = "rgba(0, 0, 0, .2)"
+        document.body.style.color = "white"
+
+        if (!auto) localStorage.setItem("tema_rodape", 0)
+
+        get("ShowButton").value = "🌞"
+
+    } else {
+
+        get("entradas").style.backgroundColor = "#f2f2f2"
+        document.body.style.color = "black"
+
+        if (!auto) localStorage.setItem("tema_rodape", 1)
+        get("ShowButton").value = "🌛"
+    }
+
+    if (!auto) mudar_tema(true)
+}
+
+mudar_tema(true)
